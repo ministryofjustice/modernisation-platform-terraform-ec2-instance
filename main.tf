8fcc6c116e78fe2bd83fbaace20ab4c801530791
Rich Green <richard.green@digital.justice.gov.uk>
2025-01-15 10:21:00 +0000

    Use distinct function to ensure no duplicate policies are attached

--- main.tf
+++ main.tf
@@ -348,9 +348,9 @@ resource "aws_iam_role" "this" {
 
 # IAM role policy attachment
 resource "aws_iam_role_policy_attachment" "this" {
-  count      = length(concat(["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"], var.instance_profile_policies))
+  count      = length(distinct(concat(["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"], var.instance_profile_policies)))
   role       = aws_iam_role.this.name
-  policy_arn = element(concat(["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"], var.instance_profile_policies), count.index)
+  policy_arn = element(distinct(concat(["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"], var.instance_profile_policies)), count.index)
 }
 
 resource "aws_iam_role_policy" "ssm_params_and_secrets" {
